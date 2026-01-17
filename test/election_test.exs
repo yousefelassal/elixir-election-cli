d4265102fdcb784710ef4395857ad54af2dea5ff
Yousef Elassal <yousef.elassal@shezlong.com>
2026-01-17 18:40:06 +0200

    Refactor tests to use setup block for election context

--- test/election_test.exs
+++ test/election_test.exs
@@ -2,20 +2,23 @@ defmodule ElectionTest do
   use ExUnit.Case
   doctest Election
 
-  test "updating election name" do
+  setup do
     election = %Election{}
-    updated_election = Election.update(election, "n City Mayor")
+    {:ok, election: election}
+  end
+
+  test "updating election name", ctx do
+    updated_election = Election.update(ctx.election, "n City Mayor")
     assert updated_election.name == "City Mayor"
   end
 
-  test "adding a candidate" do
-    election = %Election{}
-    updated_election = Election.update(election, "a Jane Doe")
+  test "adding a candidate", ctx do
+    updated_election = Election.update(ctx.election, "a Jane Doe")
     assert Enum.any?(updated_election.candidates, fn c -> c.name == "Jane Doe" end)
   end
 
-  test "voting for a candidate" do
-    election = %Election{}
+  test "voting for a candidate", ctx do
+    election = ctx.election
     election = Election.update(election, "a John Smith")
     candidate = hd(election.candidates)
     election = Election.update(election, "v #{candidate.id}")
